b44fe1bc7885dc08efd6b8ddc7735b12f796b171
Ravnoor Gill <ravnoor@gmail.com>
2023-10-12 11:32:35 -0400

    remove packaging

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import shutil
 import subprocess
 import sys
 import time
-from packaging.version import Version
 from functools import cmp_to_key
 
 import setuptools
@@ -106,10 +105,8 @@ class CMakeBuild(build_ext):
                 + ", ".join(e.name for e in self.extensions)
             )
 
-        cmake_version = Version(
-            re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-        )
-        if cmake_version.base_version < "3.10.0":
+        cmake_version = re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+        if cmake_version < "3.10.0":
             raise RuntimeError("CMake >= 3.10.0 is required")
 
         for ext in self.extensions:
